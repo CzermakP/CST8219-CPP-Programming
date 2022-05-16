c35019462ba91b1157e5ca1b2f92496e171021f6
Patrick Czermak <45134641+CzermakP@users.noreply.github.com>
2022-05-16 16:13:13 -0400

    Added lab 8

---
+++ Lab 8/PatrickCzermak_Lab8/Lab8.cpp
@@ -0,0 +1,47 @@
+﻿#include<cmath>
+#include "Lab8.h"
+
+/*
+ * Class:     Lab8
+ * Method:    calculateMean
+ * Signature: ([I)D
+ */
+ //JNIEXPORT jdouble JNICALL Java_Lab8_calculateMean(JNIEnv*, jobject, jintArray); 
+
+JNIEXPORT jdouble JNICALL Java_Lab8_calculateMean(JNIEnv* env, jobject obj, jintArray array) {
+	int arrayLength = env->GetArrayLength(array);
+	jint* elements = (env)->GetIntArrayElements(array, NULL);
+	double sum = 0, mean = 0, standardDeviation = 0;
+
+	for (int i = 0; i < arrayLength; i++) {
+		sum += elements[i];
+	}
+	mean = sum / arrayLength;
+
+	env->ReleaseIntArrayElements(array, elements, 0);
+	return mean;
+}
+
+
+/*
+ * Class:     Lab8
+ * Method:    calculateSTDDev
+ * Signature: ([I)D
+ */
+JNIEXPORT jdouble JNICALL Java_Lab8_calculateSTDDev(JNIEnv* env, jobject obj, jintArray array) {
+	int arrayLength = env->GetArrayLength(array);
+	jint* elements = (env)->GetIntArrayElements(array, NULL);
+	double sum = 0, mean = 0, standardDeviation = 0;
+
+	for (int i = 0; i < arrayLength; i++) {
+		sum += elements[i];
+	}
+	mean = sum / arrayLength;
+
+	for (int i = 0; i < arrayLength; i++) {
+		standardDeviation += pow(elements[i] - mean, 2);
+	}
+
+	env->ReleaseIntArrayElements(array, elements, 0);
+	return standardDeviation;
+}
